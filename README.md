d3eee1b085fae6b9162ec9583823c944e611b5ba
J Zhou <j.zhou1995@qq.com>
2019-02-07 22:24:44 +0000

    baseline runable

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # MLP Project
 This is our Workspace for MLP project. Check the [google docs](https://docs.google.com/document/d/1E2p1h7s7PhCX-CSWkcq4TnxyZOLhmIXH6-iZFe6VzN4/edit?usp=sharing) for our weekly TODO list. Cheers. 🍺
+
+## How to use
+1. Download the tarball of data from [Google Drive](https://drive.google.com/file/d/161iKccsFBqHAiuvRU1AP-6x89z07xEqN/view?usp=sharing). I have processed the data already.
+2. unzip the tarball `tar -xzf data.tar.gz`, then get a folder named `data`.
+3. move the `data` folder into the ROOT path of our porject.
+3. run the SHELL scripts in the folder `scripts`.
---
+++ scripts/test_local.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+#######################
+## RESET YOUR ROOT PATH
+#######################
+export ROOT_DIR=~/mlp_project
+export DATA_DIR=$ROOT_DIR/data/finished_files
+export BASELINE_CODE_DIR=$ROOT_DIR/pointer-generator
+export LOG_DIR=$ROOT_DIR/logs
+export EXP_DIR=$ROOT_DIR/exps
+export EXP_NAME=myexperiment
+
+########
+## TRAIN
+########
+python $BASELINE_CODE_DIR/run_summarization.py\
+	--mode=train\
+	--data_path=$DATA_DIR/chunked/train_*\
+	--vocab_path=$DATA_DIR/vocab\
+	--log_root=$LOG_DIR\
+	--exp_name=$EXP_DIR/$EXP_NAME
+
+########
+## EVAL
+########
+python $BASELINE_CODE_DIR/run_summarization.py\
+	--mode=eval\
+	--data_path=$DATA_DIR/chunked/val_*\
+	--vocab_path=$DATA_DIR/vocab\
+	--log_root=$LOG_DIR\
+	--exp_name=$EXP_DIR/$EXP_NAME
+
+#########
+## DECODE
+#########
+python $BASELINE_CODE_DIR/run_summarization.py\
+	--mode=decode\
+	--data_path=$DATA_DIR/chunked/val_*\
+	--vocab_path=$DATA_DIR/vocab\
+	--log_root=$LOG_DIR\
+	--exp_name=$EXP_DIR/$EXP_NAME
