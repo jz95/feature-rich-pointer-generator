6652515569d4d5001de58dc96db3e1028da26cb2
J Zhou <j.zhou1995@qq.com>
2019-03-16 19:02:22 +0000

    msg

--- scripts/local_train_n_eval.sh
+++ scripts/local_train_n_eval.sh
@@ -4,18 +4,16 @@
 #####################
 export ROOT_DIR=~/mlp_project
 export BASELINE_CODE_DIR=$ROOT_DIR/pointer-generator
-export DATA_DIR=$ROOT_DIR/data/finished
+export DATA_DIR=$ROOT_DIR/data/finished_files
 export LOG_DIR=$ROOT_DIR/logs
 # give a name to your experiment
-export EXP_NAME=pos_concate
+export EXP_NAME=test
 
 # VERY IMPORTANT
-export POS_METHOD=concate
-export CHAR_METHOD=no
+export POS_METHOD=no
+export CHAR_METHOD=concate
 
 # Activate the relevant virtual environment:
-
-source /home/${STUDENT_ID}/miniconda3/bin/activate mlp
 
 python $BASELINE_CODE_DIR/run_summarization.py\
     --mode=train\
@@ -26,7 +24,7 @@ python $BASELINE_CODE_DIR/run_summarization.py\
     --how_to_use_pos=$POS_METHOD\
     --how_to_use_char=$CHAR_METHOD & 
 
-sleep 600   # no hurry to run eval 
+sleep 60   # no hurry to run eval 
 
 ##################
 ## EVAL CONCURRENT
