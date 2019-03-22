ba8ddb285dcf21b67bade19acc77b221b0dec92d
JZ <32164281+JZ95@users.noreply.github.com>
2019-03-22 14:49:13 +0000

    Update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -2,10 +2,9 @@
 #####################
 ##### OUR OWN SETTING
 #####################
-export ROOT_DIR=~/mlp_project
-export DATA_DIR=$ROOT_DIR/data/finished_files
-export VOCAB_DIR=$ROOT_DIR/vocab
-export LOG_DIR=$ROOT_DIR/logs
+export DATA_DIR=/path/to/you/data
+export VOCAB_DIR=/path/to/you/vocab
+export LOG_DIR=/path/to/your/experiment/logs
 # give a name to your experiment
 export EXP_NAME=test
 
@@ -16,7 +15,7 @@ export CHAR_METHOD=concate
 # Activate the relevant virtual environment:
 
 frpg_run --mode=train\
-    --data_path=$DATA_DIR/chunked/train_*\
+    --data_path=$DATA_DIR/train_*\
     --vocab_path=$VOCAB_DIR\
     --log_root=$LOG_DIR\
     --exp_name=$EXP_NAME\
